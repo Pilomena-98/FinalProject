3dd2b8a0ab1e152b009517d09ffd6bab172a59b7
caasichak <44483577+caasichak@users.noreply.github.com>
2025-08-18 17:22:24 +0000

    creacion de base 100% bien

--- drawSQL-mysql-export-2025-08-18.sql
+++ drawSQL-mysql-export-2025-08-18.sql
@@ -2,7 +2,7 @@ CREATE TABLE `user`(
     `user_Id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
     `last_Name` VARCHAR(255) NOT NULL,
-    `phone` INT NOT NULL,
+    `phone` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `age` INT NOT NULL,
     `gender` VARCHAR(255) NOT NULL,
@@ -14,13 +14,13 @@ CREATE TABLE `user`(
     `passwords` VARCHAR(255) NOT NULL
 );
 CREATE TABLE `trades`(
-    `trade_Id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user_Id` INT NOT NULL,
+    `trade_Id` VARCHAR(255)  NOT NULL PRIMARY KEY,
+    `user_Id` INT UNSIGNED NOT NULL,
     `date` DATE NOT NULL,
     `time` TIME NOT NULL,
     `type` VARCHAR(255) NOT NULL,
     `quantity` INT NOT NULL,
-    `stock_Id` VARCHAR(255) NOT NULL
+    `stock_Id` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `market_prices`(
     `price_Id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -30,10 +30,10 @@ CREATE TABLE `market_prices`(
     `low` DECIMAL(8, 2) NOT NULL,
     `open` DECIMAL(8, 2) NOT NULL,
     `volume` INT NOT NULL,
-    `stock_Id` BIGINT NOT NULL
+    `stock_Id` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `stock`(
-    `stock_Id` INT NOT NULL,
+    `stock_Id` INT UNSIGNED NOT NULL,
     `ticker` VARCHAR(255) NOT NULL,
     `stock_Name` VARCHAR(255) NOT NULL,
     PRIMARY KEY(`stock_Id`)
@@ -43,4 +43,4 @@ ALTER TABLE
 ALTER TABLE
     `trades` ADD CONSTRAINT `trades_stock_id_foreign` FOREIGN KEY(`stock_Id`) REFERENCES `stock`(`stock_Id`);
 ALTER TABLE
-    `market_prices` ADD CONSTRAINT `market_prices_stock_id_foreign` FOREIGN KEY(`stock_Id`) REFERENCES `stock`(`ticker`);
+    `market_prices` ADD CONSTRAINT `market_prices_stock_id_foreign` FOREIGN KEY(`stock_Id`) REFERENCES `stock`(`stock_Id`);
