2a6bb168a5c4e6d036511864153cb0c579e75db3
Morgan Porcayo <morganporcayo@Morgans-MacBook-Air.local>
2025-08-19 16:32:27 -0600

    update queries

--- DataBasePF/Scripts.sql
+++ DataBasePF/Scripts.sql
@@ -2,7 +2,7 @@
 SELECT * FROM current_prices;
 
 -- Acciones por activo del usuario
-CALL get_current_shares(1402944639);
+CALL get_current_shares(1402944639); -- grafica
 
 -- Resumen del portafolio del usuario
 CALL get_portafolio_summary(1402944639);
@@ -12,3 +12,9 @@ SELECT get_portafolio_value(1402944639) AS "Total Portafolio Value";
 
 -- Ganancia total del portafolio del usuario
 SELECT get_portafolio_profit(1402944639) AS "Portafolio Profit";
+
+-- Historial de compra/venta de acciones del usuario
+CALL get_purchase_sale_history(1402944639, "buy");
+
+-- Hisotrial del valor del portafolio
+CALL get_portafolio_history(1402944639);
